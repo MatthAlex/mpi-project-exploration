04f9a00943abe90e336a22a6dcd3628e0f74e4ff
Matthew Alexandrakis <matt.alexandrakis@gmail.com>
2025-01-10 14:08:53 +0000

    Implement more enums; refactor mpi variable

--- src/enums.f90
+++ src/enums.f90
@@ -1,6 +1,8 @@
 module enums
    implicit none (type, external)
    private
+   !> Cartesian directions
+   integer, public, parameter :: X_DIR = 0, Y_DIR = 1, Z_DIR = 2
    !> Named constants for the 6 directions in a 3D domain.
    integer, public, parameter :: D_WEST = 1, D_EAST = 2, D_SOUTH = 3, D_NORTH = 4, D_LOW = 5, D_HIGH = 6
    !> Named constants for Boundary Conditions.
--- src/grid_module.f90
+++ src/grid_module.f90
@@ -1,18 +1,17 @@
 module grid_module
    use mpi, only: MPI_Cart_coords, MPI_Cart_create, MPI_Cart_shift, MPI_Comm_rank, MPI_Comm_size, MPI_Dims_create
    use mpi, only: MPI_COMM_WORLD, MPI_SUCCESS
+   use enums, only: X_DIR, Y_DIR, Z_DIR, D_WEST, D_EAST, D_SOUTH, D_NORTH, D_LOW, D_HIGH, PERIODIC
    implicit none (type, external)
    private
    public :: initialize_MPI_grid
 
-   !> Cartesian directions
-   integer, parameter :: X_DIR = 0, Y_DIR = 1, Z_DIR = 2
    !> Number of cores alongside the cardinal Cartesian directions
    integer :: dims(3) = [0, 0, 0]
    !> Number of dimensions for the Cartesian topology decomposition
    integer, parameter :: ndims = 3
    !> Periodic Boundaries
-   logical :: periods(3)
+   logical :: is_periodic(3)
    logical, parameter :: reorder = .true.
 
    !> This rank
@@ -49,11 +48,11 @@ contains
       call MPI_Dims_create(original_comsize, ndims, dims, ierr)
       if (ierr /= MPI_SUCCESS) error stop "Error creating dimensions"
 
-      ! Set `periods` to boundary-appropriate values
+      ! Set `is_periodic` to boundary-appropriate values
       call set_periodic_boundaries(boundaries)
 
       ! Create the new Cartesian communicator
-      call MPI_Cart_create(original_comm, ndims, dims, periods, reorder, comm_cart, ierr)
+      call MPI_Cart_create(original_comm, ndims, dims, is_periodic, reorder, comm_cart, ierr)
       if (ierr /= MPI_SUCCESS) error stop "Error creating Cartesian communicator"
 
       ! Probe the size of the Cartesian communicator
@@ -70,7 +69,7 @@ contains
       call MPI_Cart_coords(comm_cart, my_rank, ndims, my_coordinates, ierr)
       if (ierr /= MPI_SUCCESS) error stop "Error getting cartesian coordinates"
 
-      ! Determine our nearest neighbors in all 6 directions
+      ! Determine rank's nearest neighbors in all 6 directions
       call get_neighbors()
    end subroutine initialize_MPI_grid
 
@@ -79,31 +78,26 @@ contains
    !> Sets module variables: west, east, north, south, low, high
    subroutine get_neighbors()
       integer :: ierr
-      ! Call each shift along X, Y, Z axes, and get all 6 neighbours. Shift is + and -
+      ! Call each shift along X, Y, Z axes, and get all 6 neighbours. Shift is ±1
       call MPI_Cart_shift(comm=comm_cart, direction=X_DIR, disp=1, rank_source=west, rank_dest=east, ierror=ierr)
       call MPI_Cart_shift(comm=comm_cart, direction=Y_DIR, disp=1, rank_source=south, rank_dest=north, ierror=ierr)
       call MPI_Cart_shift(comm=comm_cart, direction=Z_DIR, disp=1, rank_source=low, rank_dest=high, ierror=ierr)
       if (ierr /= MPI_SUCCESS) error stop "Error in Cartesian shift"
    end subroutine get_neighbors
 
-   !> Sets the periodic boundaries based on an array of boundary codes.
-   !>
-   !> bc_types   6-element array of boundary codes for each face:
-   !> bc_types(1)=west, bc_types(2)=east, bc_types(3)=south,
-   !> bc_types(4)=north, bc_types(5)=low, bc_types(6)=high.
-   !> A code of 0 indicates periodic in that face/dimension.
+   !> Sets the periodic boundaries based on an array of boundary types.
    subroutine set_periodic_boundaries(bc_types)
       integer, intent(in) :: bc_types(6)
       ! Initialize to No Periodic Boundaries by default
-      periods = .false.
-      ! x-direction is periodic if west and east are 0
-      periods(1) = (bc_types(1) == 0 .and. bc_types(2) == 0)
+      is_periodic = .false.
+      ! x-direction is periodic if both west and east are periodic
+      is_periodic(X_DIR) = (bc_types(D_WEST) == PERIODIC .and. bc_types(D_EAST) == PERIODIC)
 
-      ! y-direction is periodic if south and north are 0
-      periods(2) = (bc_types(3) == 0 .and. bc_types(4) == 0)
+      ! y-direction is periodic if both south and north are periodic
+      is_periodic(Y_DIR) = (bc_types(D_SOUTH) == PERIODIC .and. bc_types(D_NORTH) == PERIODIC)
 
-      ! z-direction is periodic if low and high are 0
-      periods(3) = (bc_types(5) == 0 .and. bc_types(6) == 0)
+      ! z-direction is periodic if both low and high are periodic
+      is_periodic(Z_DIR) = (bc_types(D_LOW) == PERIODIC .and. bc_types(D_HIGH) == PERIODIC)
 
    end subroutine set_periodic_boundaries
 
