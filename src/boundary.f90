6d549c33f3b146b723969c1ef57adcdae1e46a57
matthalex <matthaios.alexandrakis@gre.ac.uk>
2026-05-26 14:27:29 +0100

    Remove lib_mpi_parameters entirely

--- src/boundary.f90
+++ src/boundary.f90
@@ -5,7 +5,6 @@ module boundary
    use mpi_f08, only: MPI_Abort, MPI_COMM_WORLD
    use lib_mpi_precision, only: sp
    use mpi_domain_types, only: mpi_domain_t
-   use lib_mpi_parameters, only: boundaries, nx => num_cells_x, ny => num_cells_y, nz => num_cells_z
    use lib_mpi_enums, only: D_WEST, D_EAST, D_SOUTH, D_NORTH, D_LOW, D_HIGH, PERIODIC, DIRICHLET, NEUMANN
    implicit none(type, external)
    private
